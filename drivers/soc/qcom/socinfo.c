578a80f406cf3edd04525ce3d120434c951f46bd
lrinQVQ <cqlrin@gmail.com>
2017-04-28 01:06:32 +0800

    soc: check MSM8994 V1 Support

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009-2015,2017, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -209,6 +210,8 @@ static union {
 	struct socinfo_v9 v9;
 	struct socinfo_v10 v10;
 } *socinfo;
+
+static int msm8994v1;
 
 static struct msm_soc_info cpu_of_id[] = {
 
@@ -1149,6 +1152,9 @@ static int __init socinfo_init_sysfs(void)
 	struct soc_device *soc_dev;
 	struct soc_device_attribute *soc_dev_attr;
 
+	if (msm8994v1 == 1)
+	panic("MSM8994 V1 no longer supported");
+	
 	if (!socinfo) {
 		pr_err("%s: No socinfo found!\n", __func__);
 		return -ENODEV;
@@ -1287,6 +1293,15 @@ static void socinfo_print(void)
 	}
 }
 
++static inline void check_msm8994_version(void)
+{
+	if (SOCINFO_VERSION_MAJOR(socinfo->v1.version) == 1) {
+		pr_err("MSM8994 V1 no longer supported\n");
+		msm8994v1 = 1;
+		WARN_ON(1);
+	}
+}
+
 int __init socinfo_init(void)
 {
 	static bool socinfo_init_done;
@@ -1371,6 +1386,9 @@ int __init socinfo_init(void)
 	arch_read_hardware_id = msm_read_hardware_id;
 	socinfo_init_done = true;
 
+	if (cur_cpu == MSM_CPU_8994)
+	check_msm8994_version();
+	
 	return 0;
 }
 subsys_initcall(socinfo_init);
